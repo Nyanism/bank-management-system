b3d598b5c79654154ea0dd2313a823f0ebe8c23c
Nyanism <ojjstuff@gmail.com>
2023-01-12 16:50:31 +0800

    Implement header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,13 @@
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Cognizant Bank</h1>
+      </header>
+    </>
+  );
+};
+
+export default Header;
